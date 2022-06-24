0c4c1a31d9be242c48355581d1cd49c0b7ab4b0b
Max <makism.mush21@gmail.com>
2022-06-24 18:15:38 +0300

    Второ задание

---
+++ Task2/Program1.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ввести первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввести второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввести третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberB > max)
+{
+    int max = numberB;
+    Console.WriteLine("$Максимально число: {max}.");
+}
+else
+{
+    Console.WriteLine("$Максимальное число:{max}");
+}
