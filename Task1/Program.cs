ff41608cb9e7b868bbe5bff7d913f7cf12962726
Max <makism.mush21@gmail.com>
2022-06-24 13:08:19 +0300

    task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите превое число: ");
+int numberMax = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberMin = Convert.ToInt32(Console.ReadLine());
+if (numberMax > numberMin)
+{
+    Console.WriteLine($"Минимальное число:{numberMin}.");
+    Console.WriteLine($"Максимальное число:{numberMax}.");
+}
+else
+{
+    Console.WriteLine($"Минимальное число:{numberMax}.");
+    Console.WriteLine($"Максимальное число:{numberMin}.");
+}
