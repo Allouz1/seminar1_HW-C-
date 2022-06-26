db8a55d929266025a20b2c6406bc454255ad4401
Max <makism.mush21@gmail.com>
2022-06-26 18:26:07 +0300

    done task4

---
+++ Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ввести лимит: ");
+int lim = Convert.ToInt32(Console.ReadLine());
+int number = 1;
+
+while (number < lim)
+{
+    if((number % 2) == 0) Console.Write(number + " ");
+    number++;
+}
+
+Console.Write($"|{lim}");
